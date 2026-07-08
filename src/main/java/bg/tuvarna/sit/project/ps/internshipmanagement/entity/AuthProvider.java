6649f80eb6cff8f33cc8d2f0af7543e8d012bba4
Bobozavr <Shurindex1544@gmail.com>
2026-07-08 12:39:35 +0300

    feat: add auth provider and company registration request model

---
+++ src/main/java/bg/tuvarna/sit/project/ps/internshipmanagement/entity/AuthProvider.java
@@ -0,0 +1,7 @@
+package bg.tuvarna.sit.project.ps.internshipmanagement.entity;
+
+public enum AuthProvider {
+    LOCAL,             //admin
+    UNIVERSITY,         //student
+    COMPANY_PROVIDER        //Company passed verification
+}
